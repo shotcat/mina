13eaec8449060b595fcf782be1982ba8a3558daf
248434199@qq.com <gaoyuandong@gaoyuandong-pc>
2015-07-17 09:24:45 +0800

    添加字节数组与对象之间的转换工具类

---
+++ src/main/java/chat/util/ByteUtils.java
@@ -0,0 +1,58 @@
+package chat.util;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+/***
+ * 字节数组与对象之间的转换工具类
+ * 
+ * @author gaoyuandong
+ *
+ */
+public class ByteUtils {
+
+	
+	/***
+	 * 对象转换成字节数组
+	 * @param object 要转换成字节数组的对象
+	 * @return 转成成的字节数组
+	 */
+	public static byte[] object2Bytes(Object object) {
+		try {
+			ByteArrayOutputStream arrayOutputStream = new ByteArrayOutputStream();
+			ObjectOutputStream objectOutputStream = new ObjectOutputStream(arrayOutputStream);
+			objectOutputStream.writeObject(object);
+			objectOutputStream.flush();
+			arrayOutputStream.writeTo(arrayOutputStream);
+			byte[] bytes = arrayOutputStream.toByteArray();
+			objectOutputStream.close();
+			arrayOutputStream.close();
+			return bytes;
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return null;
+	}
+	
+	/****
+	 * 将字节数组转换成对象
+	 * @param <T> 要转换的对象类型
+	 * @param bytes 要转换的字节数组
+	 * @return 转换成的对象
+	 */
+	@SuppressWarnings("unchecked")
+	public static <T> T  bytes2Object(byte[] bytes, Class<T> clazz) {
+		try {
+			ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(bytes);
+			ObjectInputStream objectInputStream = new ObjectInputStream(byteArrayInputStream);
+			return (T)objectInputStream.readObject();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+}
---
+++ src/main/java/test/MyTest.java
@@ -0,0 +1,14 @@
+package test;
+
+import java.io.FileOutputStream;
+import java.io.ObjectOutputStream;
+
+import org.apache.mina.proxy.utils.ByteUtilities;
+
+public class MyTest {
+
+	
+	public static void main(String[] args) throws Exception {
+	
+	}
+}
